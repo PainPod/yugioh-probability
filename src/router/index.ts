b91cb2c89be62980cb4bab3cca7ea54306193d4a
PainPod <grou.nico@gmail.com>
2025-11-29 01:21:50 +0900

    lib_1129_02

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 // src/router/index.ts
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import TopPage from '@/pages/TopPage.vue';
-import CalculatorPage from '@/pages/CalculatorPage.vue';
-import DeckPage from '@/pages/DeckPage.vue';
+import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
+import TopPage from "@/pages/TopPage.vue";
+import CalculatorPage from "@/pages/CalculatorPage.vue";
+import DeckPage from "@/pages/DeckPage.vue";
 
 const routes: RouteRecordRaw[] = [
-  { path: '/', name: 'top', component: TopPage },
-  { path: '/calculator', name: 'calculator', component: CalculatorPage },
-  { path: '/deck', name: 'deck', component: DeckPage },
+  { path: "/", name: "top", component: TopPage },
+  { path: "/calculator", name: "calculator", component: CalculatorPage },
+  { path: "/deck", name: "deck", component: DeckPage },
 ];
 
 export const router = createRouter({
